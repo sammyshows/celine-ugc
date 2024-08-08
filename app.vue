a9cc12dfb069da0e8ea78ecd010a7fd7917ad352
Sam McCarthy <samrmccarthy6@gmail.com>
2024-08-09 08:41:48 +1000

    Meta info for site

--- app.vue
+++ app.vue
@@ -3,3 +3,17 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup>
+useHead({
+  title: 'UGC with Celine',
+  meta: [
+    { name: 'description', content: 'Melbourne-based UGC creator that brings brands to life with authentic, captivating content.' },
+    { property: 'og:title', content: 'UGC with Celine' },
+    { property: 'og:description', content: 'Melbourne-based UGC creator that brings brands to life with authentic, captivating content.' },
+    { property: 'og:image', content: 'https://ugcwithceline.com/images/polaroid-collage.webp' },
+    { property: 'og:url', content: 'https://www.ugcwithceline.com' },
+    { property: 'og:type', content: 'website' }
+  ]
+});
+</script>
